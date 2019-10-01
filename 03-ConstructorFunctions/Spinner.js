1091668a34e63716d623aa84ce509be0bafb6f75
Magesh Kuppan <tkmagesh77@gmail.com>
2019-10-01 11:27:39 +0530

    sort implementation - assignment

--- 03-ConstructorFunctions/Spinner.js
+++ 03-ConstructorFunctions/Spinner.js
@@ -13,3 +13,15 @@ function Spinner(){
 	
 	//this -> returned by default
 }
+
+function Spinner(){
+	this.__counter__ = 0;
+}
+
+Spinner.prototype.up = function(){
+	return ++this.__counter__;
+};
+
+Spinner.prototype.down = function(){
+	return --this.__counter__;
+};
---
+++ 04-FP/products.js
@@ -0,0 +1,63 @@
+var products = [
+    {id : 7, name : 'Pen', cost : 40, units : 20, category : 'stationary'},
+    {id : 2, name : 'Ten', cost : 80, units : 10, category : 'grocery'},
+    {id : 9, name : 'Den', cost : 20, units : 40, category : 'stationary'},
+    {id : 4, name : 'Len', cost : 60, units : 50, category : 'stationary'},
+    {id : 5, name : 'Zen', cost : 50, units : 30, category : 'grocery'}
+]
+
+//sort, filter, groupBy
+
+function log(title, fn){
+    console.group(title);
+    fn();
+    console.groupEnd();
+}
+
+log('Default List', function(){
+    console.table(products);
+});
+
+log('Sort', function(){
+    log('Default Sort [ products by id] ', function(){
+        function sortProductsById(){
+            for(var i=0; i < products.length -1 ; i++)
+                for(var j = i+1; j < products.length; j++)
+                    if (products[i].id > products[j].id){
+                        var temp = products[i];
+                        products[i] = products[j];
+                        products[j] = temp;
+                    }
+        }
+        sortProductsById();
+        console.table(products);
+    });
+
+    log('Any list by any attribute', function(){
+        function sort(){
+            for(var i=0; i < products.length -1 ; i++)
+                for(var j = i+1; j < products.length; j++)
+                    if (products[i].id > products[j].id){
+                        var temp = products[i];
+                        products[i] = products[j];
+                        products[j] = temp;
+                    }
+        }
+        log('Products by cost', function(){
+            //sort(...)
+            console.table(products);
+        });
+
+        log('Products by category', function(){
+            //sort(...)
+            console.table(products);
+        });
+    });
+});
+
+log('Filter', function(){
+    log('Default Filter [ stationary products ]', function(){
+        //filterStationaryProducts();
+        console.table(products);
+    });
+});
