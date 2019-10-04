4838aa086cf1c78f39f74123c62cad2d731f5ef2
Magesh Kuppan <tkmagesh77@gmail.com>
2019-10-04 12:16:04 +0530

    app server implementation

--- 10-web-server/server.js
+++ 10-web-server/server.js
@@ -1,13 +1,16 @@
 const http = require('http'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    url = require('url');
 
 // req - IncomingMessage (class) inherits from ReadableStream
 // res - ServerResponse (class) inherits from WritableStream
 
 const server = http.createServer((req, res) => {
     console.log(`A new connection is established - ${req.url}`);
-    const resourceName = req.url === '/' ? 'index.html' : req.url;
+    const resource = req.url === '/' ? 'index.html' : req.url,
+        urlObj = url.parse(resource),
+        resourceName = urlObj.pathname;
     const resourceFullName = path.join(__dirname, resourceName);
     if (!fs.existsSync(resourceFullName)){
         res.statusCode = 404;
---
+++ 11-app-server/server.js
@@ -0,0 +1,22 @@
+const http = require('http'),
+    url = require('url'),
+    querystring = require('querystring'),
+    calculator = require('./calculator');
+
+const server = http.createServer((req, res) => {
+    const urlObj = url.parse(req.url),
+        queryData = querystring.parse(urlObj.query);
+    console.log(req.url);
+    if (urlObj.pathname !== '/calculator'){
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    const { op, x, y } = queryData;
+    const result = calculator[op](parseInt(x), parseInt(y));
+    res.write(result.toString());
+    res.end();
+});
+
+server.listen(9090);
+server.on('listening', () => console.log('app server listening on 9090'));
